de46848fe06e3fff1262adfaac1c82fb328d8846
MokshanandaReddy <mokshananda14102001@gmail.com>
2023-05-10 06:37:04 +0530

    Tested the restructured backend

---
+++ src/main/java/com/roomup/backend/controller/AdminController.java
@@ -0,0 +1,29 @@
+package com.roomup.backend.controller;
+
+import com.roomup.backend.model.Admin;
+import com.roomup.backend.repository.AdminRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class AdminController {
+    @Autowired
+    private AdminRepository adminRepository;
+
+    @PostMapping("/admin")
+    Admin newAdmin(@RequestBody Admin newAdmin)
+    {
+        return adminRepository.save(newAdmin);
+    }
+
+    @GetMapping("/admins")
+    List<Admin> getAllAdmins()
+    {
+        // return "Hello World";
+        return adminRepository.findAll();
+    }
+
+}
---
+++ src/main/java/com/roomup/backend/model/Student.java
@@ -0,0 +1,98 @@
+package com.roomup.backend.model;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue
+    private Long studentID;
+    private String name;
+    @Temporal(TemporalType.DATE)
+    private Date dob;
+    private String gender;
+    private String username;
+    private String room;
+    private String admissionID;
+    private String email;
+    public Student() {
+    }
+
+    public Student(String name, Date dob, String gender, String username, String room, String admissionID, String email) {
+        this.name = name;
+        this.dob = dob;
+        this.gender = gender;
+        this.username = username;
+        this.room = room;
+        this.admissionID = admissionID;
+        this.email = email;
+    }
+
+    public Long getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(Long studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAdmissionID() {
+        return admissionID;
+    }
+
+    public void setAdmissionID(String admissionID) {
+        this.admissionID = admissionID;
+    }
+
+
+}
