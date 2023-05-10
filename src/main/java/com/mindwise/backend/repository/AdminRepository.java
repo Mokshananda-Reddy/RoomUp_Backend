9b2c389bc59d2b75384fac8092d76898aed0e4a0
MokshanandaReddy <mokshananda14102001@gmail.com>
2023-05-10 06:31:28 +0530

    Tested the restructured backend

--- src/main/java/com/mindwise/backend/repository/AdminRepository.java
+++
@@ -1,7 +0,0 @@
-package com.roomup.backend.repository;
-
-import com.roomup.backend.model.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepository extends JpaRepository<Admin,Long> {
-}
--- src/main/java/com/mindwise/backend/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.roomup.backend.repository;
-
-import com.roomup.backend.model.Student;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student,Long> {
-
-}
